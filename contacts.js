65f2c4a6caf24760089a18c57c36c09826f9e22c
pylypchukroman <romanpylypchuk@gmail.com>
2023-12-04 20:07:27 +0200

    added all func for contacts and switch case for all actions

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join('db', "contacts.json");
+
+const listContacts = async () => {
+    const contacts = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(contacts);
+};
+
+const getContactById = async (contactId) => {
+    const contacts = await listContacts();
+    return contacts.find(contact => contact.id === contactId) || null;
+};
+
+const removeContact = async (contactId) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [removedContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+};
+
+const addContact = async (name, email, phone) => {
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    };
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+};
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+};
