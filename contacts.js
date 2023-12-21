306a77814cd1c74fafe7c1c8a2cf9ad1e8480c3e
pylypchukroman <romanpylypchuk@gmail.com>
2023-12-21 18:31:39 +0200

    changed path from join to resolve

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join('db', "contacts.json");
+const contactsPath = path.resolve('db', "contacts.json");
 
 const listContacts = async () => {
     const contacts = await fs.readFile(contactsPath, "utf-8");
